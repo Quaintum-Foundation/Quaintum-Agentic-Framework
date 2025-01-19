23e3ff384c8d818832fc8de7a1ca11ec95f9c925
Quaitre Aiquen <chello.andrea@gmail.com>
2025-01-19 16:08:32 +0000

    feat: Epsilon-greedy action selection

--- src/agents/reinforcement_learning_agent.py
+++ src/agents/reinforcement_learning_agent.py
@@ -1,16 +1,22 @@
 import numpy as np
+import random
 
 class ReinforcementLearningAgent:
-    def __init__(self, learning_rate=0.1, discount_factor=0.9, action_space=2):
+    def __init__(self, learning_rate=0.1, discount_factor=0.9, action_space=2, epsilon=0.1):
         self.q_table = {}
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
         self.action_space = action_space
+        self.epsilon = epsilon  # Exploration rate
 
     def choose_action(self, state):
         if state not in self.q_table:
             self.q_table[state] = np.zeros(self.action_space)
-        return np.argmax(self.q_table[state])
+        
+        # Epsilon-greedy action selection
+        if random.random() < self.epsilon:
+            return random.randint(0, self.action_space - 1)  # Explore
+        return np.argmax(self.q_table[state])  # Exploit
 
     def update_q_value(self, state, action, reward, next_state):
         if state not in self.q_table:
